c70bb7ffa038ece4229165758407ceeb7bf5e6c9
jtgraham5th <jtgraham5th@gmail.com>
2022-05-17 17:36:02 -0400

    updated formatting on ViewUserCard, added send drink button and drink score to View Users as well as pictures

--- src/components/ViewUserCard.jsx
+++ src/components/ViewUserCard.jsx
@@ -8,9 +8,9 @@ import {
   IonCol,
   IonRow,
   useIonRouter,
-  IonContent
+  IonContent,
 } from "@ionic/react";
-
+import styles from "./ViewUserCard.module.scss";
 import { useRef } from "react";
 import { bag } from "ionicons/icons";
 import { addToCart } from "../store/CartStore";
@@ -41,11 +41,27 @@ const ViewUserCard = (props) => {
   return (
     <IonRow key={user.id} className="animate__animated animate__fadeIn">
       <IonCol size="12" onClick={clickAction}>
-        <IonCard className="drink-card">
-          <img src={user.image} alt="drink type" />
-          <IonCardTitle className="custom-margin-left">
-            {user.name}
-          </IonCardTitle>
+        <IonCard className={styles.userCard}>
+          <IonCol size="4">
+            <img src={user.image} alt="drink type" />
+          </IonCol>
+          <IonCol size="8" className={styles.userCardInfo}>
+            <IonRow>
+              <IonCol size="9" className={styles.profileInfo}>
+                <IonCardTitle>{user.name}</IonCardTitle>
+                <IonCardSubtitle>{user.favoriteDrink}</IonCardSubtitle>
+              </IonCol>
+              <IonCol size="3" className={styles.profileStats}>
+                <IonCol className={styles.profileStat}>
+                  <IonCardTitle>{user.drinkScore}</IonCardTitle>
+                  <IonCardSubtitle>Points</IonCardSubtitle>
+                </IonCol>
+              </IonCol>
+            </IonRow>
+            <IonButton color="main" fill="outline" expand="block">
+              Send Drink
+            </IonButton>
+          </IonCol>
         </IonCard>
       </IonCol>
 
--- src/store/UserStore.jsx
+++ src/store/UserStore.jsx
@@ -5,25 +5,74 @@ const UserStore = new Store({
     {
       id: 1,
       name: "Adam",
-      image: "https://picsum.photos/200"
+      image: "../userPhotos/m01.jpeg",
+      favoriteDrink: "Rum and Coke",
+      drinkScore: 25
     },
     {
       id: 2,
       name: "Benita",
-      image: "https://picsum.photos/200"
+      image: "/userPhotos/f01.jpeg",
+      favoriteDrink: "Mascato",
+      drinkScore: 40
 
     },
     {
       id: 3,
       name: "Chris",
-      image: "https://picsum.photos/200"
+      image: "/userPhotos/m02.webp",
+      favoriteDrink: "Tequila",
+      drinkScore: 0
 
     },
     {
       id: 4,
       name: "Daphney",
-      image: "https://picsum.photos/200"
-    }
+      image: "/userPhotos/f02.jpeg",
+      favoriteDrink: "Margarita",
+      drinkScore: 20
+    },
+    {
+      id: 5,
+      name: "Eric",
+      image: "/userPhotos/m03.jpeg",
+      favoriteDrink: "Long Island Ice Tea",
+      drinkScore: 5
+
+    },
+    {
+      id: 6,
+      name: "Felicia",
+      image: "/userPhotos/f03.jpeg",
+      favoriteDrink: "Moscow Mule",
+      drinkScore: 18
+
+    },
+    {
+      id: 7,
+      name: "George",
+      image: "/userPhotos/m04.jpeg",
+      favoriteDrink: "Beer",
+      drinkScore: 30
+
+    },
+    {
+      id: 8,
+      name: "Helen",
+      image: "/userPhotos/f04.webp",
+      favoriteDrink: "Gin & Tonic",
+      drinkScore: 10
+
+    },
+    {
+      id: 9,
+      name: "Issac",
+      image: "/userPhotos/m05.jpeg",
+      favoriteDrink: "Sex on the Beach",
+      drinkScore: 40
+
+    },
+
   ],
 });
 
